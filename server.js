525a10417d13b8a9030d0a83e9b10b3ffe63c88b
parashur <parashuram.b@comorin.co>
2021-05-13 23:53:59 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-var ffmpeg = require('fluent-ffmpeg');
+const fs = require("fs");
 app.use(express.json());
 app.use(cors());
-const fs = require("fs");
-const axios = require("axios");
-const cheerio = require("cheerio");
 const ytdl = require('ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+
+let id = 'sDLsSQf3Hc0';
+
 
 app.get('/download', (req, res) => {
     var URL = req.query.URL;
@@ -15,7 +16,7 @@ app.get('/download', (req, res) => {
     var quality = req.query.quality;
     res.header('Content-Disposition', `attachment; filename="${name}.mp4"`);
     ytdl(URL, {
-        filter: format => format.quality === quality
+        filter: format => (format.quality === quality && format['hasAudio'])
     }).pipe(res);
 });
 
@@ -24,86 +25,89 @@ app.get("/videoInfo", async function (request, response) {
     const info = await ytdl.getInfo(url);
     response.status(200).json(info);
 });
- 
 
-app.get('/mp3', async (req, res) => {
-    console.log('called');
-    var URL = req.query.URL;
-    var name = req.query.fileName;
-    var quality = req.query.quality;
-    var mp3formats = req.query.mp3formats;
-    if (mp3formats) {
-        res.setHeader(
-            'Content-Disposition',
-            `attachment; filename=${name}.mp3`,
-        );
+
+// app.get('/mp3', async (req, res) => {
+//     console.log('called');
+//     var URL = req.query.URL;
+//     var name = req.query.fileName;
+//     var quality = req.query.quality;
+//     var audioBitrate = req.query.audioBitrate;
+//     var audioQuality = req.query.audioQuality;
+//     var mp3formats = req.query.mp3formats;
+//     var itag = req.query.itag;
+//     console.log('mp3formats', audioQuality, mp3formats, itag);
+//     if (mp3formats) {
+//         res.setHeader(
+//             'Content-Disposition',
+//             `attachment; filename=${name}.mp3`,
+//         );
+//         ytdl(URL, {
+//             filter: format => format['itag'] == itag
+//         }).pipe(writeableStream);
+//     }
+// });
+
+app.get("/mp3", function (request, response) {
+    const videoURL = request.query.videoURL;
+    const itag = request.query.itag;
+    console.log('itag', itag);
+    response.header("Content-Disposition", 'attachment;\ filename="video.mp3"');
+    ytdl(videoURL, {
+        filter: format => format.itag == itag
+    }).pipe(response);
+});
+
+app.get('/downloadmp3', async (req, res) => {
+    try {
+        const {
+            URL,
+            downloadFormat,
+            quality,
+            title,
+        } = req.query;
+        console.log(URL, downloadFormat, quality, title);
+
+        res.header("Content-Disposition", 'attachment;\ filename=youloads.mp3');
+
         ytdl(URL, {
-            filter: format => (format['hasAudio'] && !format['hasVideo'] && format.audioCodec ===  mp3formats),
+            // quality: 'highestaudio',
+            filter: 'audioonly',
         }).pipe(res);
+
+
+        // if (downloadFormat === 'audio-only') {
+        //     res.setHeader(
+        //         'Content-Disposition',
+        //         `attachment; filename=${title}.mp3`,
+        //     );
+
+        //     ytdl.getInfo(URL, {
+        //         quality: 'highest'
+        //     }, function (err, info) {
+        //         const stream = ytdl.downloadFromInfo(info, {
+        //                 quality: 'highest',
+        //                 requestOptions: {
+        //                     maxRedirects: 5
+        //                 }
+        //             })
+        //             .pipe(file);
+        //     });
+
+        //     // let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
+        //     ytdl(URL, {
+        //         filter: format => format. === 'm4a' && !format.encoding
+        //     }).pipe(res);
+        // }
+    } catch (e) {
+        console.log(e);
     }
 });
 
 
-// app.get('/mp3', (req, res) => {
-
-//     var URL = req.query.URL;
-//     var fileName = req.query.fileName;
-//     var quality = req.query.quality;
-//     //set stream for conversion
-//     var stream = ytdl(URL)
 
 
-//     //set response headers
-//     res.setHeader('Content-disposition', 'attachment; filename=' + fileName + '.mp3');
-//     res.setHeader('Content-type', 'audio/mpeg');
 
-
-//     var proc = new ffmpeg({
-//         source: stream
-//     });
-
-//     proc.withAudioCodec('libmp3lame')
-//         .on("end", function (stdout, stderr) {
-//             console.log("Finished");
-//             res.download(__dirname + fileName, function (err) {
-//                 if (err) throw err;
-
-//                 fs.unlink(__dirname + fileName, function (err) {
-//                     if (err) throw err;
-//                     console.log("File deleted");
-//                 });
-//             });
-//             fs.unlink("tmp/" + fileName, function (err) {
-//                 if (err) throw err;
-//                 console.log("File deleted");
-//             });
-//         })
-//         .on("error", function (err) {
-//             console.log("an error happened: " + err.message);
-//             fs.unlink("tmp/" + fileName, function (err) {
-//                 if (err) throw err;
-//                 console.log("File deleted");
-//             });
-//         })
-//         .saveToFile(__dirname + fileName);
-
-
-// })
-
-// app.get("/download",function(request,response){
-// 	const videoURL = request.query.url;
-// 	const itag = request.query.itag;
-// 	response.header("Content-Disposition",'attachment;\ filename="video.mp4"');
-// 	ytdl(videoURL,{format: 'mp4'
-// 	}).pipe(writeableStream);
-// });
-
-
-// const writeableStream = fs.createWriteStream(`sample.mp4`);
-// writeableStream .on('finish', () => {
-//     console.log(`downloaded successfully`);
-//   });
-// our sever is listening on port 3001 if we're not in production
 
 const PORT = process.env.PORT || 3001;
 
